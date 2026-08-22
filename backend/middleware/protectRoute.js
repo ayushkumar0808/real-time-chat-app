7ac1c810297cb08341bf527797477e61adde703c
Ayush Kumar <kayush3647@gmail.com>
2026-08-23 00:43:24 +0530

    updaTE

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -3,30 +3,41 @@ import User from "../models/user.model.js";
 
 const protectRoute = async (req, res, next) => {
 	try {
-		const token = req.cookies.jwt;
+		const authHeader = req.headers.authorization;
 
-		if (!token) {
-			return res.status(401).json({ error: "Unauthorized - No Token Provided" });
+		if (!authHeader || !authHeader.startsWith("Bearer ")) {
+			return res.status(401).json({
+				error: "Unauthorized - No Token Provided",
+			});
 		}
+
+		const token = authHeader.split(" ")[1];
 
 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
 		if (!decoded) {
-			return res.status(401).json({ error: "Unauthorized - Invalid Token" });
+			return res.status(401).json({
+				error: "Unauthorized - Invalid Token",
+			});
 		}
 
 		const user = await User.findById(decoded.userId).select("-password");
 
 		if (!user) {
-			return res.status(404).json({ error: "User not found" });
+			return res.status(404).json({
+				error: "User not found",
+			});
 		}
 
 		req.user = user;
 
 		next();
 	} catch (error) {
-		console.log("Error in protectRoute middleware: ", error.message);
-		res.status(500).json({ error: "Internal server error" });
+		console.log("Error in protectRoute middleware:", error.message);
+
+		return res.status(401).json({
+			error: "Unauthorized - Invalid Token",
+		});
 	}
 };
 
