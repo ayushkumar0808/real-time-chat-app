576057680a279020de31d78d0d67975eb29a5e39
Ayush Kumar <kayush3647@gmail.com>
2026-08-23 00:39:31 +0530

    updaTE

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,17 +1,15 @@
 import jwt from "jsonwebtoken";
 
-const generateTokenAndSetCookie = (userId, res) => {
-	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-		expiresIn: "15d",
-	});
+const generateToken = (userId) => {
+	const token = jwt.sign(
+		{ userId },
+		process.env.JWT_SECRET,
+		{
+			expiresIn: "15d",
+		}
+	);
 
-	res.cookie("jwt", token, {
-		maxAge: 15 * 24 * 60 * 60 * 1000,
-		httpOnly: true,
-		sameSite: "none",
-		secure: true,
-	});
-
+	return token;
 };
 
-export default generateTokenAndSetCookie;
+export default generateToken;
